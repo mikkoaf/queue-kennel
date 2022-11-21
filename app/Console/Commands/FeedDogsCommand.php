e6ecd0b281a7b0be2b13c93488da36354c843b9e
Mikko Ala-Fossi <mikko.ala-fossi@ukko.fi>
2022-11-21 15:27:17 +0200

    Add prototype of the feed batch

---
+++ app/Console/Commands/FeedDogsCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\FeedJob;
+use App\Models\Dog;
+use Illuminate\Bus\Batch;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class FeedDogsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dogs:feed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This works an introduction to batching';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $jobs = Dog::query()->chunkMap(function (Dog $dog) {
+            return new FeedJob($dog);
+        });
+
+        $batch = Bus::batch($jobs)->then(function (Batch $batch) {
+            Log::info('The '.$batch->name.' jobs completed is now complete');
+        })->catch(function (Batch $batch, Throwable $e) {
+            Log::info($batch->name.' resulted in an exception '.$e->getMessage());
+        })->finally(function (Batch $batch) {
+            Log::info('The '.$batch->name.' batch is now complete');
+        })->name('Chow time')
+            ->dispatch();
+        return Command::SUCCESS;
+    }
+}
--- app/Jobs/FeedJob.php
+++ app/Jobs/FeedJob.php
@@ -2,8 +2,9 @@
 
 namespace App\Jobs;
 
+use App\Models\Dog;
+use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
@@ -11,17 +12,22 @@ use Illuminate\Queue\SerializesModels;
 
 /**
  * A task which is done for all the dogs in the kennel.
+ * https://laravel.com/docs/9.x/queues#defining-batchable-jobs
  */
 class FeedJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+    use Batchable;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(public Dog $dog)
     {
         //
     }
@@ -33,6 +39,11 @@ class FeedJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+        if ($this->batch()->cancelled()) {
+            // Determine if the batch has been cancelled...
+            return;
+        }
+
+
     }
 }
