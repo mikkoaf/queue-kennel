6858af24bf4734c653bbea5c689a8625a6b76af7
Mikko Ala-Fossi <mikko.ala-fossi@ukko.fi>
2022-11-21 15:39:59 +0200

    Create a food catalog

---
+++ app/Enums/FoodCatalog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * This could be an enum if PHP8.1 was used...
+ */
+class FoodCatalog
+{
+    public const BACON = 'bacon';
+
+    public const KIBBLES = 'kibbles';
+
+    public const MEATBALLS = 'meatballs';
+}
