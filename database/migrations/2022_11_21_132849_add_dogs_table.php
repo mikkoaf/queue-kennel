646fa1cfa1bf40e2533a0b0451f59bbdacd34d1c
Mikko Ala-Fossi <mikko.ala-fossi@ukko.fi>
2022-11-21 15:32:55 +0200

    Create initial database representation of dog

---
+++ database/migrations/2022_11_21_132849_add_dogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('The name the dog recognizes');
+            $table->string('favorite_food')->comment('Known favorite food');
+            $table->integer('weight')->comment('How much the dog weighs, in grams');
+            $table->integer('teeth')->comment('Number of healthy teeth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
